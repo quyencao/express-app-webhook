2b1dc02353ce7e395819784483722de94a6ce1bb
quyencao <boy20195@gmail.com>
2019-07-24 13:12:28 +0700

    fourth changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Success Change!!!' })
+    res.json({ message: 'Success Change!!' })
 });
 
 app.post("/webhooks/github", function (req, res) {

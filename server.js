5e04ca88be3d894d606f0b7eb453a42e3aef65f6
quyencao <boy20195@gmail.com>
2019-07-24 11:20:39 +0700

    refactor

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Success' })
+});
+
+app.post("/webhooks/github", function (req, res) {
+    var branch = req.body.ref;
+
+    if(branch.indexOf('master') > -1){
+        deploy(res);
+    }
+})
+  
+function deploy(res){
+    childProcess.exec(`cd ${__dirname} && ./deploy.sh`, function(err, stdout, stderr){
+        if (err) {
+            console.error(err);
+            return res.json(500);
+        }
+        res.json(200);
+    });
+}
+
+app.listen(3000, () => console.log('Server is running...'))
